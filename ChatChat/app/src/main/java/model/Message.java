775a4d78fa6ba76ae8b87803a19e4f1d22801c71
Shikang_Fan <alexpiaopg@gmail.com>
2019-09-16 18:55:25 -0400

    rewrite Message and Chatroom Model

--- ChatChat/app/src/main/java/model/Message.java
+++ ChatChat/app/src/main/java/model/Message.java
@@ -1,42 +1,53 @@
 package model;
 
+import java.util.Map;
+
 public class Message {
-    private String message;
+    private Map<String, String> message;
+    public static final String MESSAGE = "message";
+    public static final String USERNAME = "username";
+    public static final String URL = "url";
+    public static final String USER_ID = "user_id";
+    public static final String DATE = "date";
+    private String content;
     private String username;
     private String url;
     private String user_id;
     private String date;
 
-    public Message()
-    {
-        message = "";
-        username = "";
-        url = "" ;
-        user_id = "";
 
-    }
-
-    public Message(String message, String username, String url, String user_id,String date)
+    public Message(String mes, String username, String url, String user_id,String date)
     {
-        this.message = message;
-        this.user_id = user_id;
-        this.url = url;
-        this.username = username;
-        this.date = date;
+        message.put(MESSAGE, mes);
+        message.put(USERNAME, user_id);
+        message.put(URL, url);
+        message.put(USER_ID, user_id);
+        message.put(DATE, date);
+
     }
 
     public String getMessage()
     {
-        return message;
+        return message.get(MESSAGE);
     }
 
     public String getUsername()
-    { return  username;}
+    {
+        return message.get(USERNAME);
+    }
 
-    public String getUrl(){return url;}
+    public String getUserId()
+    {
+        return message.get(USER_ID);
+    }
 
-    public String getUser_id(){return user_id;}
+    public String getUrl()
+    {
+        return message.get(URL);
+    }
 
-    public String getDate(){return date;}
-
+    public String getDate()
+    {
+        return message.get(DATE);
+    }
 }

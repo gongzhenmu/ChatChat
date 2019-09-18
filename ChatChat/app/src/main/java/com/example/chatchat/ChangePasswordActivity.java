f46afb8df3d91fd3acb0813ec187400882d36fc2
charlotte <charlottedyt@gmail.com>
2019-09-18 00:36:17 -0400

    minor changes
    change log to toast

--- ChatChat/app/src/main/java/com/example/chatchat/ChangePasswordActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/ChangePasswordActivity.java
@@ -3,7 +3,6 @@ package com.example.chatchat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +15,6 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import static android.content.ContentValues.TAG;
 
 public class ChangePasswordActivity extends AppCompatActivity {
     private Button btnChange;
@@ -65,7 +63,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
                                             }
                                         });
                                     } else {
-                                        Log.d(TAG, "Error auth failed");
+                                        Toast.makeText(ChangePasswordActivity.this, "Error auth failed", Toast.LENGTH_LONG).show();
                                     }
                                 }
                             });

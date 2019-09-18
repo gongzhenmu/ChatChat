e63b55ca0433de1b7babd8d3e6c2a898d711f20d
gongzhenmu <a362296048@gmail.com>
2019-09-17 23:20:50 -0400

    Image upload functionality added

--- ChatChat/app/src/main/java/com/example/chatchat/MainPageActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/MainPageActivity.java
@@ -1,12 +1,39 @@
 package com.example.chatchat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainPageActivity extends AppCompatActivity {
+    private Button imgUpload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+        imgUpload = (Button) findViewById(R.id.MainPage_changeImg);
+
+        imgUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeImg();
+
+            }
+        });
+
+
+
+    }
+
+
+
+    private void changeImg(){
+        Intent intent = new Intent(getApplicationContext(),ImageUpload.class);
+        startActivity(intent);
+
+
+
     }
 }

a17bc50ca27b2d261ae2b79f1944ba7e848a639c
Shikang_Fan <alexpiaopg@gmail.com>
2019-09-18 02:50:45 -0400

    Create room function implemented

--- ChatChat/app/src/main/java/com/example/chatchat/MainPageActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/MainPageActivity.java
@@ -1,12 +1,90 @@
 package com.example.chatchat;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import model.Chatroom;
 
 public class MainPageActivity extends AppCompatActivity {
+    Dialog createChatDialog;
+    private FirebaseFirestore db;
+    private EditText chatnameInput;
+    private EditText descriptionInput;
+    private Spinner categorySpinner;
+
+    private String chatname = "";
+    private String description = "";
+    private String category = "";
+    private Chatroom newChatRoom;
+    private Button createButton;
+    private String date = Calendar.getInstance().getTime().toString();
+
+    private Button exploreButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+        createChatDialog = new Dialog(this);
+        exploreButton = (Button)findViewById(R.id.main_button_explore);
+        exploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPageActivity.this, CategoryActivity.class);
+                startActivity(intent);
+            }
+        });
+        db = FirebaseFirestore.getInstance();
     }
+
+    public void showCreateChat(View v) {
+
+        createChatDialog.setContentView(R.layout.popup_create_chat);
+        chatnameInput = (EditText) createChatDialog.findViewById(R.id.createchat_name_text);
+        descriptionInput = (EditText) createChatDialog.findViewById(R.id.createchat_des_text);
+        categorySpinner = (Spinner) createChatDialog.findViewById(R.id.createchat_category_spinner);
+        createButton = (Button) createChatDialog.findViewById(R.id.createchat_button_create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chatname = chatnameInput.getText().toString();
+                description = chatnameInput.getText().toString();
+                category = categorySpinner.getSelectedItem().toString();
+                newChatRoom = new Chatroom(chatname, category, "Shikang", date);
+                newChatRoom.setDescription(description);
+                db.collection("Chatroom").add(newChatRoom.getChatroom()).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        createChatDialog.dismiss();
+                        Toast.makeText(MainPageActivity.this, "created", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainPageActivity.this, "fail to create an account", Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+        createChatDialog.show();
+
+    }
+
 }
+
--- ChatChat/app/src/main/java/model/Chatroom.java
+++ ChatChat/app/src/main/java/model/Chatroom.java
@@ -1,57 +1,52 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class Chatroom {
-        private Map<String, String> chatroom;
+    private Map<String, String> chatroom = new HashMap<>();
     public static final String CHAT_NAME = "chatName";
     public static final String CATEGORY = "category";
     public static final String DESCRIPTION = "description";
     public static final String LIKES = "likes";
     public static final String CREATER = "creater_name";
     public static final String DATE = "date";
-    public static final String CHAT_ID = "chatid";
 
-        private String category;
-        private String name;
-        private String description;
-        private int likes;
-        private String creater_name;
-        private String date;
-        public Chatroom()
-        {
 
-        }
+    private String category;
+    private String chatName;
+    private String description;
+    private String likes;
+    private String creater_name;
+    private String date;
 
-        public Chatroom(String name, String category, String creater_name, String date, String chatid)
-        {
-            chatroom.put(CHAT_NAME, name);
-            chatroom.put(CATEGORY, category);
-            chatroom.put(DESCRIPTION, description);
-            chatroom.put(LIKES, "0");
-            chatroom.put(CREATER, creater_name);
-            chatroom.put(DATE, date);
-            chatroom.put(CHAT_ID, chatid);
-        }
+    public Chatroom(String name, String category, String creater_name, String date)
+    {
+        chatroom.put(CHAT_NAME, name);
+        chatroom.put(CATEGORY, category);
+        chatroom.put(LIKES, "0");
+        chatroom.put(CREATER, creater_name);
+        chatroom.put(DATE, date);
 
-        public void setDescription(String des)
-        {
-            chatroom.put(DESCRIPTION, des);
-        }
+    }
 
-        public void like()
-        {
-            int like_num = Integer.parseInt(chatroom.get(LIKES)) + 1;
-            chatroom.put(LIKES, like_num+"");
+    public void setDescription(String des)
+    {
+        chatroom.put(DESCRIPTION, des);
+    }
 
-        }
+    public void like()
+    {
+        int like_num = Integer.parseInt(chatroom.get(LIKES)) + 1;
+        chatroom.put(LIKES, like_num+"");
 
-        public String getName(){return chatroom.get(CHAT_NAME);}
-        public String getCategory(){return chatroom.get(CATEGORY);}
-        public String getCreater_name(){return chatroom.get(CREATER);}
-        public String getDescription(){return chatroom.get(DESCRIPTION);}
-        public String getLikes(){return chatroom.get(LIKES);}
-        public String getChatId(){return chatroom.get(CHAT_ID);}
+    }
+    public Map<String, String> getChatroom(){return chatroom;}
+    public String getName(){return chatroom.get(CHAT_NAME);}
+    public String getCategory(){return chatroom.get(CATEGORY);}
+    public String getCreater_name(){return chatroom.get(CREATER);}
+    public String getDescription(){return chatroom.get(DESCRIPTION);}
+    public String getLikes(){return chatroom.get(LIKES);}
 
 }

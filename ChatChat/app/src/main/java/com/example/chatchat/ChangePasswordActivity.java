e25b0d29354a9c5ab3340d9a06f8dc9baadc2416
charlotte <charlottedyt@gmail.com>
2019-09-20 14:15:13 -0400

    change uid and change password complete. added to profile page

--- ChatChat/app/src/main/java/com/example/chatchat/ChangePasswordActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/ChangePasswordActivity.java
@@ -19,33 +19,44 @@ import com.google.firebase.auth.FirebaseUser;
 public class ChangePasswordActivity extends AppCompatActivity {
     private Button btnChange;
     private FirebaseUser currentUser;
-    private EditText emailInput;
     private EditText passwordInput;
     private EditText newPasswordInput;
-    private String email;
+    private EditText newPassAgainInput;
     private String password;
     private String newPassword;
+    private String newPassAgain;
     private FirebaseAuth firebaseAuth;
+    private int newPassLenLimit = 20;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.change_password);
-        emailInput = (EditText) findViewById(R.id.email);
-        passwordInput = (EditText) findViewById(R.id.password);
-        newPasswordInput = (EditText) findViewById(R.id.newPass);
-        btnChange = (Button) findViewById(R.id.change_passWord);
+        newPassAgainInput = (EditText) findViewById(R.id.txtNewPassAgain);
+        passwordInput = (EditText) findViewById(R.id.txtPassword);
+        newPasswordInput = (EditText) findViewById(R.id.txtNewPass);
+        btnChange = (Button) findViewById(R.id.btnChange_passWord);
         firebaseAuth = FirebaseAuth.getInstance();
         currentUser = firebaseAuth.getCurrentUser();
+
         btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                email = emailInput.getText().toString().trim();
                 password = passwordInput.getText().toString().trim();
                 newPassword = newPasswordInput.getText().toString().trim();
-                if (!TextUtils.isEmpty(emailInput.getText()) && !TextUtils.isEmpty(passwordInput.getText())
+                newPassAgain = newPassAgainInput.getText().toString().trim();
+                if(newPassword.length() > newPassLenLimit){
+                    Toast.makeText(ChangePasswordActivity.this, "Error. Password too long", Toast.LENGTH_LONG).show();
+                }
+                else if(newPassword.length() == 0){
+                    Toast.makeText(ChangePasswordActivity.this, "Error. Empty password not allowed", Toast.LENGTH_LONG).show();
+                }
+                else if(!newPassword.equals(newPassAgain)){
+                    Toast.makeText(ChangePasswordActivity.this, "Error. New passwords don't match", Toast.LENGTH_LONG).show();
+                }
+                else if (!TextUtils.isEmpty(newPasswordInput.getText()) && !TextUtils.isEmpty(passwordInput.getText())
                         && !TextUtils.isEmpty(newPasswordInput.getText())){
                     AuthCredential credential = EmailAuthProvider
-                            .getCredential(email, password);
+                            .getCredential(currentUser.getEmail(), password);
                     currentUser.reauthenticate(credential)
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
@@ -58,12 +69,12 @@ public class ChangePasswordActivity extends AppCompatActivity {
                                                     Toast.makeText(ChangePasswordActivity.this, "Password updated", Toast.LENGTH_LONG).show();
                                                     launchMainPage();
                                                 } else {
-                                                    Toast.makeText(ChangePasswordActivity.this, "Error password not updated", Toast.LENGTH_LONG).show();
+                                                    Toast.makeText(ChangePasswordActivity.this, "Error. Password not updated", Toast.LENGTH_LONG).show();
                                                 }
                                             }
                                         });
                                     } else {
-                                        Toast.makeText(ChangePasswordActivity.this, "Error auth failed", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(ChangePasswordActivity.this, "Error. Authentication failed", Toast.LENGTH_LONG).show();
                                     }
                                 }
                             });
@@ -73,7 +84,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
     }
 
     private void launchMainPage() {
-        Intent intent = new Intent(ChangePasswordActivity.this, MainPageActivity.class);
+        Intent intent = new Intent(ChangePasswordActivity.this, ProfileActivity.class);
         startActivity(intent);
         finish();
     }

b7b8bcc93e1b4833ebe353d12363ef2551848c4f
siyujiang7 <jiangsiyumichael@outlook.com>
2019-09-20 20:14:06 -0400

    espresso tests for login

--- ChatChat/app/src/androidTest/java/com/example/chatchat/Activities/LoginUnitTest.java
+++ ChatChat/app/src/androidTest/java/com/example/chatchat/Activities/LoginUnitTest.java
@@ -3,22 +3,18 @@ package com.example.chatchat.Activities;
 import com.example.chatchat.MainActivity;
 import com.example.chatchat.R;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
+import androidx.test.espresso.action.TypeTextAction;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @LargeTest
@@ -31,8 +27,19 @@ public class LoginUnitTest {
             = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void login() {
+    public void mainPageButtonIsDisplayed() {
         // Type text and then press the button.
+        onView(withId(R.id.login_bt)).check(matches(withText("LOG IN WITH EMAIL")));
+        onView(withId(R.id.login_bt)).perform(click());
+    }
+
+    @Test
+    public void Login() {
+        // Type text and then press the button.
+        onView(withId(R.id.login_bt)).perform(click());
+        onView(withId(R.id.email)).perform(new TypeTextAction("user1@email.com"));
+        onView(withId(R.id.password)).perform(new TypeTextAction("12345678"));
+        onView(withId(R.id.password)).perform(closeSoftKeyboard());
         onView(withId(R.id.login_bt)).perform(click());
     }
 }

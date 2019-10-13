d1b53eed81bb6dde314203edc044db0bad63a813
Canyu Yang <yangiq199@gmail.com>
2019-10-13 19:03:22 -0400

    Search
    Change

--- ChatChat/app/src/main/java/com/example/chatchat/SearchActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/SearchActivity.java
@@ -1,7 +1,8 @@
 package com.example.chatchat;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
@@ -15,7 +16,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 
 import Adapter.ChatRoomListAdapter;
@@ -28,11 +28,12 @@ public class SearchActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ChatRoomListAdapter adapter;
     private ArrayList<Chatroom> chatrooms;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chatroom_list);
+        setContentView(R.layout.activity_search);
 
         mSearch = findViewById(R.id.search_room);
         recyclerView = findViewById(R.id.activity_search_recyclerview);
@@ -43,40 +44,43 @@ public class SearchActivity extends AppCompatActivity {
         chatrooms = new ArrayList<>();
         adapter = new ChatRoomListAdapter(chatrooms, SearchActivity.this);
         recyclerView.setAdapter(adapter);
-
+        button = findViewById(R.id.search_button);
         Query chatroomQuery = db.collection("Chatroom").whereEqualTo("chatName", mSearch.getText().toString());
         Log.d("ChatroomList", "getting query from db");
-        chatroomQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful())
-                {
-                    Log.d("ChatroomList", "getting rooms from db");
+            public void onClick(View v) {
+                Query chatroomQuery = db.collection("Chatroom").whereEqualTo("chatName", mSearch.getText().toString());
+                Log.d("ChatroomList", "getting query from db");
+                chatroomQuery.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("ChatroomList", "getting rooms from db");
 
-                    for(QueryDocumentSnapshot chatroomDoc : task.getResult())
-                    {
-                        Log.d("ChatroomList", "getting single room attributes from db");
-                        String category = (String)chatroomDoc.getData().get(Chatroom.CATEGORY);
-                        String chatName = (String)chatroomDoc.getData().get(Chatroom.CHAT_NAME);
-                        String description = (String)chatroomDoc.getData().get(Chatroom.DESCRIPTION);
-                        String likes = (String)chatroomDoc.getData().get(Chatroom.LIKES);
-                        String creater_name = (String)chatroomDoc.getData().get(Chatroom.CREATER);
-                        String date = (String)chatroomDoc.getData().get(Chatroom.DATE);
-                        String chat_id = (String)chatroomDoc.getData().get(Chatroom.CHAT_ID);
+                            for (QueryDocumentSnapshot chatroomDoc : task.getResult()) {
+                                Log.d("ChatroomList", "getting single room attributes from db");
+                                String category = (String) chatroomDoc.getData().get(Chatroom.CATEGORY);
+                                String chatName = (String) chatroomDoc.getData().get(Chatroom.CHAT_NAME);
+                                String description = (String) chatroomDoc.getData().get(Chatroom.DESCRIPTION);
+                                String likes = (String) chatroomDoc.getData().get(Chatroom.LIKES);
+                                String creater_name = (String) chatroomDoc.getData().get(Chatroom.CREATER);
+                                String date = (String) chatroomDoc.getData().get(Chatroom.DATE);
+                                String chat_id = (String) chatroomDoc.getData().get(Chatroom.CHAT_ID);
 
-                        Chatroom tempChat = new Chatroom(chatName, category, creater_name, date);
-                        tempChat.setChatId(chat_id);
-                        tempChat.setLikes(likes);
-                        tempChat.setDate(description);
-                        chatrooms.add(tempChat);
+                                Chatroom tempChat = new Chatroom(chatName, category, creater_name, date);
+                                tempChat.setChatId(chat_id);
+                                tempChat.setLikes(likes);
+                                tempChat.setDate(description);
+                                chatrooms.add(tempChat);
+                                adapter.notifyDataSetChanged();
+                            }
+                        } else {
+                            Log.d("ChatroomList", "No room found");
+                        }
                         adapter.notifyDataSetChanged();
                     }
-                }
-                else
-                {
-                    Log.d("ChatroomList", "No room found");
-                }
-                adapter.notifyDataSetChanged();
+                });
             }
         });
 

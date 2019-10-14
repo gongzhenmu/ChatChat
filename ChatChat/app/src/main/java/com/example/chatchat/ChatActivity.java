7ac94b421e7ff2557aa7b5177629914c34d0775a
siyujiang7 <jiangsiyumichael@outlook.com>
2019-10-13 21:49:02 -0400

    chat room info (#19)

--- ChatChat/app/src/main/java/com/example/chatchat/ChatActivity.java
+++ ChatChat/app/src/main/java/com/example/chatchat/ChatActivity.java
@@ -1,18 +1,8 @@
 package com.example.chatchat;
 
-import Adapter.ChatAdapter;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import model.Chatroom;
-import model.Message;
-import model.User;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +36,15 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import Adapter.ChatAdapter;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import model.Message;
+import model.User;
+
 public class ChatActivity extends AppCompatActivity {
 
     private String chatroom_id;
@@ -65,6 +64,7 @@ public class ChatActivity extends AppCompatActivity {
     private ToggleButton toggle_button_like;
     private int numberOfLikes;
     private ChatAdapter adapter;
+    private ImageButton informationbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,7 +75,7 @@ public class ChatActivity extends AppCompatActivity {
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         user_uid = firebaseUser.getUid();
 
-        rv = (RecyclerView)findViewById(R.id.activity_chat_message_recyclerView);
+        rv = (RecyclerView) findViewById(R.id.activity_chat_message_recyclerView);
         LinearLayoutManager llm = new LinearLayoutManager(ChatActivity.this);
         rv.setLayoutManager(llm);
 
@@ -83,41 +83,49 @@ public class ChatActivity extends AppCompatActivity {
         adapter = new ChatAdapter(messageArrayList, user_uid, ChatActivity.this);
         rv.setAdapter(adapter);
 
-        chatroom_id =  getIntent().getStringExtra("Chatroom_ID");
+        informationbtn = findViewById(R.id.activity_chat_button_info);
+
+        chatroom_id = getIntent().getStringExtra("Chatroom_ID");
         chatroom_name = getIntent().getStringExtra("Chatroom_NAME");
-        titleText = (TextView)findViewById(R.id.activity_chat_title);
+        titleText = (TextView) findViewById(R.id.activity_chat_title);
         titleText.setText(chatroom_name);
-        button_send = (ImageButton)findViewById(R.id.activity_chat_button_send);
-        editText = (EditText)findViewById(R.id.activity_chat_message_editText);
+        button_send = (ImageButton) findViewById(R.id.activity_chat_button_send);
+        editText = (EditText) findViewById(R.id.activity_chat_message_editText);
         editText.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                rv.scrollToPosition(adapter.getItemCount()-1);
+                rv.scrollToPosition(adapter.getItemCount() - 1);
                 return false;
             }
-    });
-
+        });
+        informationbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatActivity.this, ChatRoomInfoActivity.class);
+                intent.putExtra("Chatroom_ID", chatroom_id);
+                startActivity(intent);
+            }
+        });
         //get  username for current user
         username = "null_username";
         final DocumentReference userRf = db.collection("Users").document(user_uid);
         userRf.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if(task.isSuccessful())
-                {
+                if (task.isSuccessful()) {
                     DocumentSnapshot userDoc = task.getResult();
-                    username = (String)userDoc.getData().get(User.CHAT_NAME);
-                    imgurl = (String)userDoc.getData().get(User.IMAGE);
+                    username = (String) userDoc.getData().get(User.CHAT_NAME);
+                    imgurl = (String) userDoc.getData().get(User.IMAGE);
                 }
             }
         });
-   //     retriveMessage();
+        //     retriveMessage();
         button_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String message_content = "";
                 message_content = editText.getText().toString();
-                Long ts = System.currentTimeMillis()/1000;
+                Long ts = System.currentTimeMillis() / 1000;
                 String timestamp = ts.toString();
                 Calendar cal = Calendar.getInstance(Locale.ENGLISH);
                 cal.setTimeInMillis(ts * 1000);
@@ -203,8 +211,7 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    public void sendMessage(Message message)
-    {
+    public void sendMessage(Message message) {
         CollectionReference messageRef = db.collection("Chatroom").document(chatroom_id).
                 collection("Messages");
         messageRef.document(message.getTimestamp()).set(message.getMessage()).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -215,28 +222,25 @@ public class ChatActivity extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(ChatActivity.this, "fail to send message" , Toast.LENGTH_LONG).show();
+                Toast.makeText(ChatActivity.this, "fail to send message", Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    public void retriveMessage()
-    {
+    public void retriveMessage() {
         CollectionReference messageRef = db.collection("Chatroom").document(chatroom_id).
                 collection("Messages");
         messageRef.orderBy("timestamp", Query.Direction.ASCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful())
-                {
-                    for(QueryDocumentSnapshot doc : task.getResult())
-                    {
-                        String content = (String)doc.getData().get(Message.CONTENT);
-                        String date = (String)doc.getData().get(Message.DATE);
-                        String timestamp = (String)doc.getData().get(Message.TIMESTAMP);
-                        String url = (String)doc.getData().get(Message.URL);
-                        String user_id = (String)doc.getData().get(Message.USER_ID);
-                        String username = (String)doc.getData().get(Message.USERNAME);
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot doc : task.getResult()) {
+                        String content = (String) doc.getData().get(Message.CONTENT);
+                        String date = (String) doc.getData().get(Message.DATE);
+                        String timestamp = (String) doc.getData().get(Message.TIMESTAMP);
+                        String url = (String) doc.getData().get(Message.URL);
+                        String user_id = (String) doc.getData().get(Message.USER_ID);
+                        String username = (String) doc.getData().get(Message.USERNAME);
                         Message tmpMessage = new Message(content, username, url, user_id, date);
                         tmpMessage.setTimestamp(timestamp);
                         messageArrayList.add(tmpMessage);
@@ -248,30 +252,27 @@ public class ChatActivity extends AppCompatActivity {
 
     }
 
-    public void receiveMessage()
-    {
+    public void receiveMessage() {
         CollectionReference messageRef = db.collection("Chatroom").document(chatroom_id).
                 collection("Messages");
         messageRef.orderBy("timestamp", Query.Direction.ASCENDING);
         messageRef.addSnapshotListener(ChatActivity.this, new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
-                for(DocumentChange dc : queryDocumentSnapshots.getDocumentChanges())
-                {
-                    switch (dc.getType())
-                    {
+                for (DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()) {
+                    switch (dc.getType()) {
                         case ADDED:
-                            String content = (String)dc.getDocument().getData().get(Message.CONTENT);
-                            String date = (String)dc.getDocument().getData().get(Message.DATE);
-                            String timestamp = (String)dc.getDocument().getData().get(Message.TIMESTAMP);
-                            String url = (String)dc.getDocument().getData().get(Message.URL);
-                            String user_id = (String)dc.getDocument().getData().get(Message.USER_ID);
-                            String username = (String)dc.getDocument().getData().get(Message.USERNAME);
+                            String content = (String) dc.getDocument().getData().get(Message.CONTENT);
+                            String date = (String) dc.getDocument().getData().get(Message.DATE);
+                            String timestamp = (String) dc.getDocument().getData().get(Message.TIMESTAMP);
+                            String url = (String) dc.getDocument().getData().get(Message.URL);
+                            String user_id = (String) dc.getDocument().getData().get(Message.USER_ID);
+                            String username = (String) dc.getDocument().getData().get(Message.USERNAME);
                             Message tmpMessage = new Message(content, username, url, user_id, date);
                             tmpMessage.setTimestamp(timestamp);
                             messageArrayList.add(tmpMessage);
                             adapter.notifyDataSetChanged();
-                            rv.scrollToPosition(adapter.getItemCount()-1);
+                            rv.scrollToPosition(adapter.getItemCount() - 1);
                             break;
                         case MODIFIED:
                             break;

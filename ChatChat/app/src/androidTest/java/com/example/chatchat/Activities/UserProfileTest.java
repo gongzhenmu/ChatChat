91fa5517753ffb99e8442d32cf0d92c93456051f
gongzhenmu <a362296048@gmail.com>
2019-10-20 22:16:15 -0400

    basic userprofile test

--- ChatChat/app/src/androidTest/java/com/example/chatchat/Activities/UserProfileTest.java
+++ ChatChat/app/src/androidTest/java/com/example/chatchat/Activities/UserProfileTest.java
@@ -1,4 +1,6 @@
 package com.example.chatchat.Activities;
+import android.content.Intent;
+
 import androidx.test.rule.ActivityTestRule;
 import com.example.chatchat.R;
 import com.example.chatchat.UserProfileActivity;
@@ -9,33 +11,69 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertNotNull;
 
 public class UserProfileTest {
     @Rule
     public ActivityTestRule<UserProfileActivity> UserProfileActivityTestRule = new ActivityTestRule<>(UserProfileActivity.class);
-    private UserProfileActivity UserProfileActivity = null;
 
-
-    @Before
-    public void setUp()
-    {
-        UserProfileActivity = UserProfileActivityTestRule.getActivity();
-    }
 
     @Test
-    public void image_test()
+    public void functionality()
     {
-//        assertNotNull(onView(withId(R.id.userProfile_email)));
-//        assertNotNull(onView(withId(R.id.userProfile_image)));
-//        assertNotNull(UserProfileActivity.findViewById(R.id.userProfile_listTitle));
-//        assertNotNull(UserProfileActivity.findViewById(R.id.userProfile_Name));
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        //onView(withId(R.id.userProfile_image)).perform(click());
+        assertNotNull(onView(withId(R.id.userProfile_email)));
+        assertNotNull(onView(withId(R.id.userProfile_image)));
+        assertNotNull(onView(withId(R.id.userProfile_Name)));
+
     }
+
+//    @Test
+//    public void nameTest(){
+//        Intent intent = new Intent();
+//        intent.putExtra("UID","OZ3Q3M38JSMdIn0qiiqjcqlEY4M2");
+//        UserProfileActivityTestRule.launchActivity(intent);
+//
+//        onView(withId(R.id.userProfile_Name)).check(matches(isDisplayed()));
+//        //onView(withId(R.id.userProfile_Name)).check(matches(withText("123")));
+//        UserProfileActivityTestRule.finishActivity();
+//
+//    }
+//    @Test
+//    public void emailTest(){
+//        Intent intent = new Intent();
+//        intent.putExtra("UID","OZ3Q3M38JSMdIn0qiiqjcqlEY4M2");
+//        UserProfileActivityTestRule.launchActivity(intent);
+//
+//        onView(withId(R.id.userProfile_Name)).check(matches(isDisplayed()));
+//        //onView(withId(R.id.userProfile_email)).check(matches(withText("gongzhenmu@163.com")));
+//        UserProfileActivityTestRule.finishActivity();
+//
+//    }
+//    @Test
+//    public void name1Test(){
+//        Intent intent = new Intent();
+//        intent.putExtra("UID","B1c9PZOlFfhO5Csa9DjMlvj8k422");
+//        UserProfileActivityTestRule.launchActivity(intent);
+//
+//        onView(withId(R.id.userProfile_Name)).check(matches(isDisplayed()));
+//        //onView(withId(R.id.userProfile_Name)).check(matches(withText("user3")));
+//        UserProfileActivityTestRule.finishActivity();
+//
+//    }
+//    @Test
+//    public void email1Test(){
+//        Intent intent = new Intent();
+//        intent.putExtra("UID","B1c9PZOlFfhO5Csa9DjMlvj8k422");
+//        UserProfileActivityTestRule.launchActivity(intent);
+//
+//        onView(withId(R.id.userProfile_Name)).check(matches(isDisplayed()));
+//        //onView(withId(R.id.userProfile_email)).check(matches(withText("user3@email.com")));
+//        UserProfileActivityTestRule.finishActivity();
+//
+//    }
+
 }
